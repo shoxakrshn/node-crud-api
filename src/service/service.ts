0add7f428d2c5385c795bf0cca55984223641539
shoxa <sllx.ok@gmail.com>
2024-02-02 22:30:55 +0500

    feat: create service for db

---
+++ src/service/service.ts
@@ -0,0 +1,45 @@
+import { Worker } from 'node:cluster';
+import { UserType, MessageType } from '../types/types';
+
+export class UserService {
+  public getUser = (worker: Worker, db: UserType[], message: MessageType) => {
+    const user: UserType = db.find(({ id }) => id === message.userId);
+
+    if (user) {
+      worker.send(user);
+    } else {
+      worker.send(null);
+    }
+  };
+
+  public getAll = (worker: Worker, db: UserType[]) => {
+    worker.send(db);
+  };
+
+  public delete = (worker: Worker, db: UserType[], message: MessageType) => {
+    const userIndex = db.findIndex(({ id }) => id === message.userId);
+
+    if (userIndex !== -1) {
+      db.splice(userIndex, 1);
+      worker.send(userIndex);
+    } else {
+      worker.send(-1);
+    }
+  };
+
+  public post = (worker: Worker, db: UserType[], message: MessageType) => {
+    db.push(message.data);
+    worker.send(message.data);
+  };
+
+  public put = (worker: Worker, db: UserType[], message: MessageType) => {
+    const userIndex = db.findIndex(({ id }) => id === message.userId);
+    if (userIndex !== -1) {
+      const updatedUser: UserType = { ...db[userIndex], ...message.data };
+      db[userIndex] = updatedUser;
+      worker.send(updatedUser);
+    } else {
+      worker.send(-1);
+    }
+  };
+}
---
+++ src/service/serviceHandler.ts
@@ -0,0 +1,22 @@
+import { Worker } from 'node:cluster';
+import { users } from '../utils/usersDb';
+import { MessageType } from '../types/types';
+import { UserService } from './service';
+
+const db = users;
+
+const service = new UserService();
+
+const handlerMessage = {
+  GET_ALL: service.getAll,
+  GET: service.getUser,
+  POST: service.post,
+  PUT: service.put,
+  DELETE: service.delete,
+};
+
+export const serviceHandler = (worker: Worker, message: MessageType) => {
+  const service = handlerMessage[message.type as keyof typeof handlerMessage];
+
+  service(worker, db, message);
+};
