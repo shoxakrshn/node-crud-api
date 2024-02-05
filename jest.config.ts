7c396522c90423295e11b3890549bb058393054d
shoxa <sllx.ok@gmail.com>
2024-02-05 17:24:28 +0500

    feat: install and configure jest

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['/node_modules/'],
+  testMatch: ['**/?(*.)+(test).ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  restoreMocks: true,
+  resetMocks: true,
+  moduleDirectories: ['node_modules', '<rootDir>/src'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+};
+
+export default config;
