121956f7c23f3b8817e5d2954f329898a0c8f34a
shoxa <sllx.ok@gmail.com>
2024-02-16 20:35:43 +0500

    refactor: add additional check for isValidBody

--- src/utils/isValidBody.ts
+++ src/utils/isValidBody.ts
@@ -1,6 +1,12 @@
 import type { BodyType } from '../types/types';
 
 export const isValidBody = (userData: BodyType): userData is BodyType => {
+  const validKeys = ['username', 'age', 'hobbies'];
+  const keys = Object.keys(userData);
+  if (!keys.every((key) => validKeys.includes(key)) || keys.length !== validKeys.length) {
+    return false;
+  }
+
   return (
     typeof userData === 'object' &&
     typeof userData.username === 'string' &&
